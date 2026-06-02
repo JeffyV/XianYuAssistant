7b0852cbc787493787d41b110d68105c2949decd
Jeffy <824117564@qq.com>
2026-06-02 17:35:23 +0800

    modify:前端编译

---
+++ 前端编译.bat
@@ -0,0 +1,12 @@
+@echo off
+echo cd XianYuAssistant\vue-code
+cd /d D:\Coding\Jeffy_WorkSpace\XianYuAssistant\vue-code
+
+echo npm run build:spring
+npm run build:spring
+
+echo.
+echo ======================
+echo success!
+echo ======================
+pause
